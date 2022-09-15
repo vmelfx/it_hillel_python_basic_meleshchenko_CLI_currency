c1ee5d33bd5c5deb177519232127479e5bad95a3
vmelfx <vladimir.meleshchenko@outllok.com>
2022-09-16 01:51:24 +0300

    Commit №2 16.09.2022 Added:  - Added cli interface, using click library.  - Added error-handling: handled Invalid currency and invalid date errors.  - Added statickmethod "get_list_of_currencies" for checking all available currencies supported by the api Changed:  - Changed variable name "date" to "exchange_date" for better readability.

--- currency_api.py
+++ currency_api.py
@@ -1,37 +1,80 @@
 import requests
+import click
+from datetime import datetime
 
 
 class NbuCurrency:
     print_decorator_upper = "--------------"
     print_decorator_lower = "=============="
 
-    def __init__(self, currency, date=None):
+    def __init__(self, currency, exchange_date=None):
         self.currency = currency
-        self.date = date
+        self.exchange_date = exchange_date
 
     def data_printer(self):
-        currency_data = self.get_currency_data(self.currency, self.date)
+        currency_data = self.get_currency_data(self.currency, self.exchange_date)
+        currency_rate = currency_data[0]['rate']
+        currency_id = currency_data[0]['cc']
         print(self.print_decorator_upper)
-        print(self.currency)
-        print(currency_data)
+        print(currency_id)
+        print(currency_rate)
         print(self.print_decorator_lower)
 
-    def get_currency_data(self, currency, date):
-        if date is None:
+    def get_currency_data(self, currency, exchange_date):
+        if exchange_date is None:
             api_url_currency_data = f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?" \
                                     f"valcode={currency}&json"
         else:
-            date = date.replace('-', '')
             api_url_currency_data = f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?" \
-                                    f"valcode={currency}&date={date}&json"
+                                    f"valcode={currency}&date={exchange_date}&json"
         response_currency = requests.request("GET", api_url_currency_data)
         if response_currency.status_code == requests.codes.ok:
             raw_currency_data = response_currency.json()
-            currency = raw_currency_data[0]['rate']
-            return currency
+            return raw_currency_data
+        else:
+            print("Error:", response_currency.status_code, response_currency.text)
+
+    @staticmethod
+    def get_list_of_currencies():
+        api_url_currency_data = f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json"
+        response_currency = requests.request("GET", api_url_currency_data)
+        if response_currency.status_code == requests.codes.ok:
+            raw_currency_data = response_currency.json()
+            raw_currencies_list = [element['cc'] for element in raw_currency_data]
+            currencies_list = [element.lower() for element in raw_currencies_list]
+            return currencies_list
         else:
             print("Error:", response_currency.status_code, response_currency.text)
 
 
-eur_to_uan_data = NbuCurrency(currency='USD', date='2013-01-01')
-eur_to_uan_data.data_printer()
+@click.command()
+@click.argument("currency")
+@click.option("--exchange_date", "-d")
+def main(currency, exchange_date):
+    try:
+        currencies_list = NbuCurrency.get_list_of_currencies()
+        currency = currency.lower()
+        if currency not in currencies_list:
+            raise NameError
+        if exchange_date is not None:
+            exchange_date = datetime.strptime(exchange_date, '%Y-%m-%d').date()
+            if 1996 < exchange_date.year <= datetime.now().year:
+                exchange_date = datetime.strftime(exchange_date, '%Y-%m-%d')
+                exchange_date = exchange_date.replace('-', '')
+                usd_to_uan_data = NbuCurrency(currency=currency, exchange_date=exchange_date)
+                usd_to_uan_data.data_printer()
+            else:
+                raise ValueError
+        else:
+            usd_to_uan_data = NbuCurrency(currency=currency, exchange_date=exchange_date)
+            usd_to_uan_data.data_printer()
+    except ValueError:
+        print(f'Invalid exchange_date {exchange_date}!')
+        raise SystemExit(1)
+    except NameError:
+        print(f"{currency.upper()} currency isn't supported!")
+        raise SystemExit(1)
+
+
+if __name__ == "__main__":
+    main()
