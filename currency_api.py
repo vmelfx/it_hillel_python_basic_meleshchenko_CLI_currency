2770a538290fdfeae5f911383f6f73863485f959
vmelfx <vladimir.meleshchenko@outllok.com>
2022-09-20 19:07:54 +0300

    Add comments
        - Add comments for methods and main() function.
        - Add general exception handling - if any mistake during execution will occur - user will get "System error" message and program will be terminated.

--- currency_api.py
+++ currency_api.py
@@ -12,6 +12,9 @@ class NbuCurrency:
         self.exchange_date: str = exchange_date
 
     def data_printer(self):
+        """
+        This method is responsible for data output. It takes all required data and prints it into console.
+        """
         currency_data: list = self.get_currency_data(self.currency, self.exchange_date)
         currency_rate: str = currency_data[0]['rate']
         currency_id: str = currency_data[0]['cc']
@@ -22,6 +25,13 @@ class NbuCurrency:
 
     @staticmethod
     def get_currency_data(currency, exchange_date) -> list:
+        """
+        Method responsible for getting UAH to other currencies exchange rate data from api.
+        :param currency: currency for which you want to know the rate
+        :param exchange_date: optional parameter. The date on which you want to receive the exchange rate
+        for the specified currency.
+        :return: raw (not formatted) data from json. This data will be processed in the data_printer method
+        """
         if exchange_date is None:
             api_url_currency_data: str = f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?" \
                                     f"valcode={currency}&json"
@@ -42,6 +52,13 @@ class NbuCurrency:
 
     @staticmethod
     def get_list_of_currencies():
+        """
+        This method on each startup asks api about all supported currencies.
+        Then, depending on user input program will decide: can it get data from api or not.
+        For example, if user input is BCH, program will check, that this currency doesn't exist
+        in list returned by api and program will return error.
+        :return: list of supported currencies by api
+        """
         api_url_currency_data: str = f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json"
         response_currency = requests.request("GET", api_url_currency_data)
         if response_currency.status_code == requests.codes.ok:
@@ -65,8 +82,17 @@ def main(currency, exchange_date):
         currencies_list = NbuCurrency.get_list_of_currencies()
         currency: str = currency.upper()
         if currency not in currencies_list:
+            # If entered by the user currency doesn't exist in returned by api list of supported currencies
+            # NameError will be raised.
             raise NameError
         if exchange_date is not None:
+            """
+            If exchange_date parameter is presented the following logic will check that given data is
+            in correct format and fits the limitations:
+            1. The entered date is not less than 1996 - the minimum supported data by api.
+            2. The entered date is not greater than current year.
+            If these requirements are not met, than the ValueError will be raised.
+            """
             exchange_date = datetime.strptime(exchange_date, '%Y-%m-%d').date()
             if 1996 < exchange_date.year <= datetime.now().year:
                 exchange_date = datetime.strftime(exchange_date, '%Y-%m-%d')
@@ -83,6 +109,9 @@ def main(currency, exchange_date):
         raise SystemExit(1)
     except NameError:
         print(f"{currency} currency isn't supported!")
+        raise SystemExit(1)
+    except Exception:
+        print("System error")
         raise SystemExit(1)
 
 
